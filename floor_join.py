7e206d08ae5aa4791fcd846453524b1a7b9b07a1
travis <jty@ubuntu>
2017-08-23 08:27:49 -0700

    not that good

--- floor_join.py
+++ floor_join.py
@@ -13,6 +13,7 @@
 import os
 from PIL import Image
 from random import randint
+import time
 
 image_width = 630
 image_height = 630
@@ -31,7 +32,7 @@ output_width = image_width*output_size[1]
 
 
 def load_tiles(dir_path, num):
-    image_list = {}
+    image_list = []
     for i in range(num):
         image = Image.open(dir_path + '/%d.jpg'%(i+1))
         image_list.append(image.crop(default_box))
@@ -39,7 +40,7 @@ def load_tiles(dir_path, num):
     return image_list
 
 def generate_random_mat(tiles_num, output_size):
-    tiles_mat = []
+    tiles_mat = [0] * output_size[0]
     for i in range(output_size[0]):
         tiles_mat[i] = [0]*output_size[1]
         for j in range(output_size[1]):
@@ -47,7 +48,29 @@ def generate_random_mat(tiles_num, output_size):
                 tmp = randint(0,8)
                 if tiles_num[tmp] != 0:
                     tiles_num[tmp] -= 1
-                    tiles_mat[i][j] = (tmp, randint(0,3))
+                    tiles_mat[i][j] = (tmp+1, randint(0,3))
+                    break;
+    print(tiles_mat)
+
+    return tiles_mat
+
+def generate_fix_mat(tiles_mat, tiles_num, output_size):
+    for i in range(output_size[0]):
+        for j in range(output_size[1]):
+            if tiles_mat[i][j] != (0,0):
+                index, _ = tiles_mat[i][j]
+                tiles_num[index-1] -= 1
+    print(tiles_mat)
+
+    for i in range(output_size[0]):
+        for j in range(output_size[1]):
+            while True:
+                if tiles_mat[i][j] != (0,0):
+                    break;
+                tmp = randint(0,8)
+                if tiles_num[tmp] != 0:
+                    tiles_num[tmp] -= 1
+                    tiles_mat[i][j] = (tmp+1, randint(0,3))
                     break;
     print(tiles_mat)
 
@@ -57,11 +80,82 @@ def generate_random_mat(tiles_num, output_size):
 
 
 #(tile_index, rotate_index)
-def compose_tiles(mat):
+def compose_tiles(mat, images):
+    canvas = Image.new('RGB', (output_width, output_height))
+    l, t, r, b = 0, 0, image_width, image_height
+    for lines in mat:
+        for tile in lines:
+            index, rotate = tile
+            canvas.paste(images[index-1].rotate(tiles_rotate[rotate]), (l, t, r, b))
+            l += image_width
+            r += image_width
+        l, r = 0, image_width
+        t += image_height
+        b += image_height
+
+#    canvas.show()
+    canvas.save('output/%d.jpg'%int(time.time()), quality = 100)
+    return 0
+
+choose = 2
+
+if choose == 0:
+
+    tiles_image = load_tiles('./input', 9)
+
+    tiles_mat = generate_random_mat(tiles_num, output_size)
+
+    compose_tiles(tiles_mat, tiles_image)
 
 
-load_tiles('./input', 9)
+# fix part of the tiles
+if choose == 1:
 
-tiles_mat = generate_random_mat()
+    mat_1 = [
+            [(6,1), (9,0), (1,0), (4,3), (5,0), (9,0), (6,1), (5,0), (7,0), (8,0),
+                (7,0)],
+            [(1,0), (5,0), (8,0), (7,0), (2,3), (3,0), (7,0), (4,0), (6,2), (1,0),
+                (9,0)],
+            [(7,0), (4,0), (6,0), (3,0), (9,0), (1,0), (6,2), (8,0), (1,0), (3,0),
+                (6,2)],
+            [(2,0), (3,0), (8,0), (2,2), (5,0), (8,0), (4,1), (5,0), (9,0), (6,0),
+                (7,0)],
+            [(6,3), (5,0), (1,0), (7,0), (4,0), (6,3), (9,0), (2,1), (3,0), (7,0),
+                (8,0)]
+            ]
+    tiles_image = load_tiles('./input', 9)
 
+    compose_tiles(mat_1, tiles_image)
 
+if choose == 2:
+
+    mat_1 = [
+            [(0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (6,0), (0,0), (6,3), (0,0),
+                (0,0)],
+            [(0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (3,0), (0,0), (0,0),
+                (0,0)],
+            [(0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (6,1), (0,0), (6,2), (0,0),
+                (0,0)],
+            [(0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
+                (0,0)],
+            [(0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0), (0,0),
+                (0,0)]
+            ]
+    mat_2 = [
+            [(0,0), (0,0), (4,0), (0,0), (0,0), (0,0), (6,0), (6,3), (3,0), (0,0),
+                (0,0)],
+            [(0,0), (0,0), (0,0), (4,0), (0,0), (0,0), (6,1), (6,2), (0,0), (3,0),
+                (0,0)],
+            [(4,1), (0,0), (0,0), (0,0), (4,0), (3,0), (0,0), (0,0), (0,0), (0,0),
+                (0,0)],
+            [(0,0), (4,1), (6,0), (6,3), (0,0), (0,0), (3,0), (0,0), (0,0), (0,0),
+                (0,0)],
+            [(0,0), (0,0), (6,1), (6,2), (0,0), (0,0), (0,0), (3,0), (0,0), (0,0),
+                (0,0)]
+            ]
+
+    tiles_image = load_tiles('./input', 9)
+
+    tiles_mat = generate_fix_mat(mat_2, tiles_num, output_size)
+
+    compose_tiles(tiles_mat, tiles_image)
